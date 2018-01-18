b0b53ffaa52f5a0371838fb1d2f878d3753aaa58
Your Name <you@example.com>
2018-01-18 15:25:27 +0100

    versioDefinitiva

--- readgooglemaps.js
+++ readgooglemaps.js
@@ -16,8 +16,25 @@ function loadJSON(callback) {
 function viewJSON() {
     loadJSON(function(response) {
         /* Aqui l'objecte response representa l'objecte JSON que ens 
-           ha retornat el servidor */
-        document.getElementById("results").innerHTML = response.results[0].name;
+        ha retornat el servidor */
+        var llistat = ""; /* Amb aquesta variable el document html treurà el llistat de restaurants
+        /* aquest for serveix per repetir els elements del restaurant */
+        for(i in response.results){   
+            var restaurant = response.results[i];
+            var arrayRest = ""; 
+            /* aquest for serveix per repetir els elements de restaurant.type */
+            for(j in restaurant.types){
+                arrayRest = arrayRest + restaurant.types[j]+", ";
+            }
+            
+            llistat = llistat + "<h1>" + restaurant.name + "<img src=' "+restaurant.icon+"'>" + "</h1> <br>"
+            + "Direcció: " + restaurant.vicinity + "<br>"
+            + "Latitud: " + restaurant.geometry.location.lat + "<br>"
+            + "Longitud: " + restaurant.geometry.location.lng + "<br>"
+            + "Tipus: " + arrayRest + "<br>";
+        }
+        /* enllaçam la variable llista al document html */
+        document.getElementById("results").innerHTML = llistat;
     });
 }
 
